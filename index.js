aa8bf2dc3b497d76f8f70d1f8a7a9edc51108669
computerfan <diannaomi97@gmail.com>
2019-02-18 01:36:56 +0800

    Get the week correctly. Update default start date to adapt 18-19 S2.

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ module.exports = function (configs, callback) {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'
         },
         baseUrl: 'https://ebridge.xjtlu.edu.cn/urd/sits.urd/run',
-        year: 2018,
+        year: 2019,
         month: 2,
-        day: 26
+        day: 18
     };
     config.util.extendDeep(defaultConfigs, configs);
     config.util.setModuleDefaults('xjtlu-timetable-to-ics', defaultConfigs);
